33bd454defec42aa0559da878ac4031744eae632
oksanaozcan <mekansa83@gmail.com>
2024-06-15 20:09:49 +0300

    validation schema for register & cookies for access_token after registration

--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -4,26 +4,57 @@ import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import { RegisterFormValidationSchema } from "@/validation/register-form-validation-schema";
 import { AuthInput } from './auth-input';
+import axios from 'axios';
+import { hasCookie, setCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation'
+import { env } from 'process';
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/register`;
 
 interface RegisterFormValues {
   name: string;
   email: string;
+  password: string;
+  confirm_password: string;
 }
 
 export const RegisterForm = () => {
+  const router = useRouter();
+
   return ( 
     <Formik 
       initialValues={{
         name: '', 
         email: '',
+        password: '',
+        confirm_password: '',
       }}   
       validationSchema={ Yup.object(RegisterFormValidationSchema)}
-      onSubmit={
-        (values: RegisterFormValues) => {
-          alert(JSON.stringify(values, null, 2));
+      onSubmit={ async (values: RegisterFormValues, actions) => {        
+        try {
+          const response = await axios.post(URL, values);
+          const { access_token } = response.data;
+
+          const expireDate = new Date(new Date().setDate(new Date().getDate() + 7));
+
+          setCookie('dracaena_access_token', access_token, {expires: expireDate,
+            // secure: true ///////////////// uncomment for production https
+          });        
+          
+          if (hasCookie('dracaena_access_token')) {
+            router.push('/')
+          }
+
+          console.log('Registration successful', response);
+        } catch (error) {
+          console.error('Registration failed', error);
+        } finally {
+          actions.setSubmitting(false);
+          console.log("finally block");
+        }        
         }
-      }
-    >
+      }     
+    >     
       <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <AuthInput
           label="Name"
@@ -39,15 +70,29 @@ export const RegisterForm = () => {
           placeholder="oksanaozcan@example.com"
         />      
 
-        <button 
-          type="submit"       
-        >Submit</button>    
+        <AuthInput
+          label="Password"
+          name="password"
+          type="password"
+        />      
 
-        <button 
-          type="reset"        
-        >Reset Form</button>
+        <AuthInput
+          label="Confirm password"
+          name="confirm_password"
+          type="password"
+        /> 
+
+        <div className='flex justify-between align-middle mt-4'>
+          <button type='submit' className="bg-black hover:bg-slate-700 text-white font-bold py-2 px-4 rounded-full">
+            Submit
+          </button>
+
+          <button type='reset' className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-full shadow">
+            Reset Form
+          </button>
+        </div>       
 
       </Form>     
-    </Formik>
+    </Formik>      
   )
 } 
--- validation/register-form-validation-schema.ts
+++ validation/register-form-validation-schema.ts
@@ -13,4 +13,14 @@ export const RegisterFormValidationSchema = {
       const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       return re.test(String(value).toLowerCase())
     }),
+  password: Yup.string()
+    .min(8, 'Password must be 8 characters long')
+    .matches(/[0-9]/, 'Password requires a number')
+    .required('Required'),  
+    // .matches(/[a-z]/, 'Password requires a lowercase letter')
+    // .matches(/[A-Z]/, 'Password requires an uppercase letter')
+    // .matches(/[^\w]/, 'Password requires a symbol'),
+  confirm_password: Yup.string()
+    .oneOf([Yup.ref('password')], 'Must match "password" field value')
+    .required('Required'),
 }
